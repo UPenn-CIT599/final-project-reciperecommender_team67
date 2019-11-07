56e117cbe1b767807d380da6c020c5eb05ca7482
dsangiamo <sangiamo@seas.upenn.edu>
2019-11-07 11:41:29 -0600

    Added State enum

---
+++ src/State.java
@@ -0,0 +1,19 @@
+
+public enum State {
+	RECEIVING_INPUT("receiving"), DISPLAYING_OUTPUT("displaying");
+	
+	private String text;
+	
+	State(String text) {
+		this.text = text;
+	}
+	
+	/**
+	 * gets the text representing the current state
+	 * @return String containing the current state of the program
+	 */
+	public String getText() {
+		return text;
+	}
+
+}
