1e369f9ddbf28506210899524d4e0a8a68267c29
dsangiamo <sangiamo@seas.upenn.edu>
2019-12-02 14:10:01 -0600

    added minimum cosine similarity threshold

--- src/RecipeRecommender.java
+++ src/RecipeRecommender.java
@@ -16,23 +16,21 @@ public class RecipeRecommender {
 	 * @param recipes arraylist of recipes to choose from
 	 * @param inputIngredients a string containing space separated ingredients to use to find a similar recipe
 	 * @param numRecipes integer representing how many recipes should be returned
+	 * @param minSimilarity the similarity score threshold a recipe must excede to be included
 	 * @return Arraylist of numRecipes recipes that are most similar to inputIngredients
 	 */
-	public static ArrayList<Recipe> returnRecipe(ArrayList<Recipe> recipes, String inputIngredients, int numRecipes) {
+	public static ArrayList<Recipe> returnRecipe(ArrayList<Recipe> recipes, String inputIngredients, int numRecipes, double minSimilarity) {
 		DataPreparation dataPrep = new DataPreparation("en-token.bin", "en-pos-maxent.bin");
 		ArrayList<Double> cosSimilarity = new ArrayList<Double>();
 		// get cosine similarity for each recipe
-		int errorCounter = 0;
 		for (Recipe r : recipes) {
 			String currRecipeIngredients = dataPrep.makeContiniousString(r.getIngredients());
 			try {
 				cosSimilarity.add(1 - new CosineDistance().apply(currRecipeIngredients.toLowerCase(), inputIngredients));
 			} catch(Exception e) {
 				cosSimilarity.add(-2.0);
-				errorCounter++;
 			}
 		}
-		System.out.println(errorCounter);
 
 		ArrayList<Recipe> topRecipes = new ArrayList<Recipe>();
 		ArrayList<Double> topSimilarity = new ArrayList<Double>();
@@ -40,7 +38,10 @@ public class RecipeRecommender {
 
 		for (int i = 0; i < recipes.size(); i++) {
 			// automatically add the first recipe
-			if (i==0) {
+			if (cosSimilarity.get(i) < minSimilarity) {
+				continue;
+			}
+			else if (topRecipes.isEmpty()) {
 				topRecipes.add(recipes.get(i));
 				topSimilarity.add(cosSimilarity.get(i));
 			} else {
@@ -83,29 +84,7 @@ public class RecipeRecommender {
 			}
 		}
 
-		//DELETE
-		for (Double d : topSimilarity) {
-			System.out.println(d);
-		}
-
 		return topRecipes;
 	}
-
-
-//	 Using this main method for testing purposes DELETE EVENTUALLY
-	  public static void main(String[] args) {
-//		  RecipeRecommender rr = new
-//		  RecipeRecommender();
-//
-//		  String[] example = new String[3]; example[0] = "prepared pizza crust";
-//		  example[1] = "sausage patty"; example[2] = "eggs";
-//
-//		  rr.returnRecipe(example); System.out.println(example);
-		  String string1 = "chicken potato rice zucchini carrot";
-		  String string2 = "zucchini";
-
-		  System.out.println((1 - new CosineDistance().apply(string2, string1)));
-	  }
-
 
 }
